2aa6c8604aa0abffe47f52d7f334640b7aa15b6b
Zyf <zyfarok@gmail.com>
2021-01-05 18:45:01 +0100

    Fix compilation errors in simple_tm.scala

--- simple_tm.scala
+++ simple_tm.scala
@@ -1,81 +1,124 @@
-// import stainless.collection._
-// import stainless.lang._
-// import stainless.annotation._
-// import stainless.equations._
+import stainless.collection._
+import stainless.lang._
+import stainless.annotation._
+import stainless.equations._
+import java.sql.Time
 
-type Process = BigInt
-type MemoryObject = BigInt
-type TimeStamp = BigInt
+object TMSystem {
+    type Process = BigInt
+    type MemoryObject = BigInt
+    type TimeStamp = BigInt
 
-sealed abstract class Operation(obj: MemoryObject) {
-    def o = obj
-}
-case class Read(obj: MemoryObject) extends Operation(obj)
-case class Write(obj: MemoryObject) extends Operation(obj)
+    def isValidSet[K](keys: List[K]): Boolean = {
+        keys match {
+            case k :: ks => !ks.contains(k) && isValidSet(ks)
+            case Nil() => true
+        }
+    }
 
-case class Transaction(ops: Seq[Operation])
+    case class LMap[K,V](keys: List[K], values: List[V]) {
+        require(isValidSet(keys) && keys.length == values.length)
 
-case class ProcessState(
-    currentTx: BigInt,
-    currentOp: BigInt,
-    currentSubOp: BigInt,
-    readSet: Map[MemoryObject, TimeStamp],
-    writeSet: Set[MemoryObject]
-) {
-    def nextSubOp(): ProcessState =
-        ProcessState(currentTx, currentOp, currentSubOp + 1, readSet, writeSet)
+        def +(t: (K,V)): LMap[K,V] = t match {
+            case (k,v) => this.+(k,v)
+        }
+
+        def +(k: K, v: V): LMap[K,V] =
+            if(keys.contains(k))
+                LMap(keys, values.updated(keys.indexOf(k), v))
+            else
+                LMap(Cons(k, keys), Cons(v, values))
         
-    def nextOp(): ProcessState =
-        ProcessState(currentTx, currentOp + 1, 0, readSet, writeSet)
+        def map(f: (K,V) => (K,V)): LMap[K,V] = {
+            (keys, values) match {
+                case (Cons(k, ks), Cons(v, vs)) => {
+                    val x = f(k, v)
+                    val xs = LMap(ks,vs).map(f)
+                    LMap(Cons(x._1, xs.keys), Cons(x._2, xs.values))
+                }
+                case (Nil(), Nil()) => this
+            }
+        }
+    }
+
+    object LMap {
+        def emptyMap[A,B] = LMap(Nil[A](),Nil[B]())
+    }
+
+    sealed abstract class Operation {
+        def o: MemoryObject
+    }
+    case class Read(obj: MemoryObject) extends Operation {
+        def o = obj
+    }
+    case class Write(obj: MemoryObject) extends Operation {
+        def o = obj
+    }
+
+    case class Transaction(ops: List[Operation])
+
+    case class ProcessState(
+        currentTx: BigInt,
+        currentOp: BigInt,
+        currentSubOp: BigInt,
+        readSet: LMap[MemoryObject, TimeStamp],
+        writeSet: Set[MemoryObject]
+    ) {
+        def nextSubOp(): ProcessState =
+            ProcessState(currentTx, currentOp, currentSubOp + 1, readSet, writeSet)
+            
+        def nextOp(): ProcessState =
+            ProcessState(currentTx, currentOp + 1, 0, readSet, writeSet)
+            
+        def nextTx(): ProcessState =
+            ProcessState(currentTx + 1, 0, 0, LMap.emptyMap[MemoryObject, TimeStamp], Set())
+
+        def abortTx(): ProcessState =
+            ProcessState(currentTx, -1, 0, readSet, writeSet)
+
+        def restartTx(): ProcessState =
+            ProcessState(currentTx, 0, 0, LMap.emptyMap[MemoryObject, TimeStamp], Set())
+
+        def addToReadSet(obj: MemoryObject, ts: TimeStamp): ProcessState =
+            ProcessState(currentTx, currentOp, currentSubOp, readSet + (obj -> ts), writeSet)
+
+        def addToWriteSet(obj: MemoryObject): ProcessState =
+            ProcessState(currentTx, currentOp, currentSubOp, readSet, writeSet + obj)
+    }
+
+    val startProcess = ProcessState(0, 0, 0, LMap.emptyMap[MemoryObject, TimeStamp], Set())
+
+    case class SystemState(
+        txQueues: LMap[Process, List[Transaction]],
+        procStates: LMap[Process, ProcessState],
+        dirtyObjs: Set[MemoryObject],
+        lockedObjs: Set[MemoryObject],
+        objTimeStamps: LMap[MemoryObject, TimeStamp]
+    ) {
+        def updateState(proc: Process, state: ProcessState): SystemState =
+            SystemState(txQueues, procStates + (proc -> state), dirtyObjs, lockedObjs, objTimeStamps)
         
-    def nextTx(): ProcessState =
-        ProcessState(currentTx + 1, 0, 0, Map(), Set())
+        def markDirty(obj: MemoryObject): SystemState =
+            SystemState(txQueues, procStates, dirtyObjs + obj, lockedObjs, objTimeStamps)
 
-    def abortTx(): ProcessState =
-        ProcessState(currentTx, -1, 0, readSet, writeSet)
+        def cleanObjects(writeSet: Set[MemoryObject]): SystemState =
+            SystemState(txQueues, procStates, dirtyObjs -- writeSet, lockedObjs, objTimeStamps)
 
-    def restartTx(): ProcessState =
-        ProcessState(currentTx, 0, 0, Map(), Set())
+        def acquireLock(obj: MemoryObject): SystemState =
+            SystemState(txQueues, procStates, dirtyObjs, lockedObjs + obj, objTimeStamps)
+        
+        def releaseLocks(writeSet: Set[MemoryObject]): SystemState =
+            SystemState(txQueues, procStates, dirtyObjs, lockedObjs -- writeSet, objTimeStamps)
+        
+        def updateTimestamps(writeSet: Set[MemoryObject]): SystemState =
+            SystemState(txQueues, procStates, dirtyObjs, lockedObjs,
+                    objTimeStamps.map{case (o, ts) => if (writeSet.contains(o)) (o, ts + 1) else (o, ts)})
+    }
 
-    def addToReadSet(obj: MemoryObject, ts: TimeStamp): ProcessState =
-        ProcessState(currentTx, currentOp, currentSubOp, readSet + (obj -> ts), writeSet)
-
-    def addToWriteSet(obj: MemoryObject): ProcessState =
-        ProcessState(currentTx, currentOp, currentSubOp, readSet, writeSet + obj)
-}
-
-val startProcess = ProcessState(0, 0, 0, Map(), Set())
-
-case class SystemState(
-    txQueues: Map[Process, Seq[Transaction]],
-    procStates: Map[Process, ProcessState],
-    dirtyObjs: Set[MemoryObject],
-    lockedObjs: Set[MemoryObject],
-    objTimeStamps: Map[MemoryObject, TimeStamp]
-) {
-    def updateState(proc: Process, state: ProcessState): SystemState =
-        SystemState(txQueues, procStates + (proc -> state), dirtyObjs, lockedObjs, objTimeStamps)
-    
-    def markDirty(obj: MemoryObject): SystemState =
-        SystemState(txQueues, procStates, dirtyObjs + obj, lockedObjs, objTimeStamps)
-
-    def cleanObjects(writeSet: Set[MemoryObject]): SystemState =
-        SystemState(txQueues, procStates, dirtyObjs -- writeSet, lockedObjs, objTimeStamps)
-
-    def acquireLock(obj: MemoryObject): SystemState =
-        SystemState(txQueues, procStates, dirtyObjs, lockedObjs + obj, objTimeStamps)
-    
-    def releaseLocks(writeSet: Set[MemoryObject]): SystemState =
-        SystemState(txQueues, procStates, dirtyObjs, lockedObjs -- writeSet, objTimeStamps)
-    
-    def updateTimestamps(writeSet: Set[MemoryObject]): SystemState =
-        SystemState(txQueues, procStates, dirtyObjs, lockedObjs,
-            objTimeStamps.map{case (o,ts) =>  if(writeSet.contains(o)) (o, ts + 1) else (o, ts)})
-}
-
-def startSystem(txQueues: Map[Process, Seq[Transaction]]): SystemState = {
-    var procStates = txQueues.mapValues(_ => startProcess)
-    var objs = txQueues.values.flatten.flatten(tx => tx.ops).map(op => op.o).toSet
-    var objTimeStamps = objs.map(o => (o, BigInt(0))).toMap
-    SystemState(txQueues, procStates, Set(), Set(), objTimeStamps)
+    def startSystem(txQueues: LMap[Process, List[Transaction]]): SystemState = {
+        var procStates = LMap(txQueues.keys, txQueues.values.map(_ => startProcess))
+        var objs = txQueues.values.flatMap(x => x).flatMap(tx => tx.ops).map(op => op.o).toSet.toList
+        var objTimeStamps = LMap(objs, objs.map(_ => BigInt(0)))
+        SystemState(txQueues, procStates, Set(), Set(), objTimeStamps)
+    }
 }
